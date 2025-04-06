d68a03b832a5060fe92c7e76abb5fd74cce7892a
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2025-04-06 18:51:28 +0530

    Create Q3.cpp

---
+++ MINOR-ASSIGNMENT-2/Q3.cpp
@@ -0,0 +1,11 @@
+Q.Let say, void setValues(int, int); and void getValues( ); are the public member functions for the class calculateAreaf ... g;. Write the C++ statements to call both the
+functions using object(s) of that class.
+Answer:
+// Creating an object of the class
+calculateAreaf obj;
+
+// Calling the setValues() function
+obj.setValues(10, 20); // Pass integer values as arguments
+
+// Calling the getValues() function
+obj.getValues(); // Displays or retrieves the values
