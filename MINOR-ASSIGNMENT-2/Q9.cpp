8a2b9ada4ac075bcaa125d2ad6fbd6afe38d010a
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2025-04-06 23:16:29 +0530

    Create Q9.cpp

---
+++ MINOR-ASSIGNMENT-2/Q9.cpp
@@ -0,0 +1,16 @@
+9.Let us pretend the speed of a spriteBee is 200 pixels per second. Calculate the amount of time the
+Bee will take to cross the entire width of the screen that is, 2000 pixels wide. Also calculate the bit
+rate per second (i.e. bps) of the spriteBee, if 1 pixel contains 8 bits
+
+Answer>>
+Time to Cross the Screen:
+  Screen width: 2000 pixels
+  Speed: 200 pixels/second
+  Time 𝑇  = Screen Width/Speed
+  𝑇 = 2000 / 200 = 10 seconds
+
+Bit Rate per Second:
+1 pixel contains 8 bits.
+Speed: 200 pixels/second
+Bit rate bps = Pixels per second × Bits per pixel
+bps = 200 × 8 = 1600bps
