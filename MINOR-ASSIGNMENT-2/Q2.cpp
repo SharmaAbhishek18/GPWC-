b8edfc13cc7b4dfe98fefbfe2e3fe738ebeb957e
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2025-04-06 18:50:21 +0530

    Update Q2.cpp

--- MINOR-ASSIGNMENT-2/Q2.cpp
+++ MINOR-ASSIGNMENT-2/Q2.cpp
@@ -1,10 +1,3 @@
 2.Assume that you have a class as calculateAreaf ... g;, Write the C++ statement to declare 4 objects of that class.
 ANSWER>>
-// Creating an object of the class
-calculateAreaf obj;
-
-// Calling the setValues() function
-obj.setValues(10, 20); // Pass integer values as arguments
-
-// Calling the getValues() function
-obj.getValues(); // Displays or retrieves the values
+calculateArea obj1, obj2, obj3, obj4;
