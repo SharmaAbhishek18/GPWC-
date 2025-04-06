54d1bb521492f1f07e98459f1452558f63e85404
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2025-04-06 18:49:40 +0530

    Create Q2.cpp

---
+++ MINOR-ASSIGNMENT-2/Q2.cpp
@@ -0,0 +1,10 @@
+2.Assume that you have a class as calculateAreaf ... g;, Write the C++ statement to declare 4 objects of that class.
+ANSWER>>
+// Creating an object of the class
+calculateAreaf obj;
+
+// Calling the setValues() function
+obj.setValues(10, 20); // Pass integer values as arguments
+
+// Calling the getValues() function
+obj.getValues(); // Displays or retrieves the values
